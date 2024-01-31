3fd67cf145828d4ce395513f57e8e01e40442e64
yuvalom <92787451+yuvalom@users.noreply.github.com>
2024-01-31 19:32:49 +0200

    first tag v1.0.0

--- dog.go
+++ dog.go
@@ -1,9 +1,14 @@
 package godog
 
 import (
+	"fmt"
 	"strings"
 )
 
 func WhenGrownUp(s string) string {
 	return "When the puppy grows up is says: " + strings.ToUpper(s)
+}
+
+func From1_0() string {
+	fmt.Println("I am from version v1.0.0")
 }
